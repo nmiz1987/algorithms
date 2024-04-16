75f3db7fd8015ad3daa464ca5ee35f78ea59ba4e
Netanel Mizrahi <nmiz1987@gmail.com>
2024-04-16 08:24:39 +0300

    create project

---
+++ BinarySearchList/BinarySearchList.ts
@@ -0,0 +1,17 @@
+export default function BinarySearchList(arr: number[], target: number): boolean {
+  let lo = 0;
+  let hi = arr.length;
+
+  do {
+    const m = Math.floor((lo + hi) / 2);
+    const c = arr[m];
+    if (c === target) {
+      return true;
+    } else if (c > target) {
+      hi = m;
+    } else {
+      lo = m + 1;
+    }
+  } while (lo < hi);
+  return false;
+}
---
+++ LinearSearchList/LinearSearchList.ts
@@ -0,0 +1,8 @@
+export default function LinearSearch(arr: number[], target: number): boolean {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) {
+      return true;
+    }
+  }
+  return false;
+}
