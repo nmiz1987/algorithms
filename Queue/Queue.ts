13e2e11bcab769dabd14b126679ab9bf563dc24f
Netanel Mizrahi <nmiz1987@gmail.com>
2024-04-17 08:14:47 +0300

    add queue

---
+++ Queue/Queue.ts
@@ -0,0 +1,61 @@
+type Node<T> = {
+  value: T;
+  next?: Node<T>;
+};
+
+export default class Queue<T> {
+  public length: number;
+  private head?: Node<T>;
+  private tail?: Node<T>;
+
+  constructor() {
+    this.length = 0;
+    this.head = this.tail = undefined;
+  }
+
+  enqueue(item: T): void {
+    const node = { value: item } as Node<T>;
+    this.length++;
+    if (!this.tail) {
+      this.head = this.tail = node;
+      return;
+    }
+
+    this.tail.next = node;
+    this.tail = node;
+  }
+
+  deque(): T | undefined {
+    if (!this.head) {
+      return undefined;
+    }
+    this.length--;
+    const head = this.head;
+    this.head = this.head.next;
+    head.next = undefined;
+
+    if (this.length === 0) {
+      this.tail = undefined;
+    }
+
+    return head.value;
+  }
+
+  peek(): T | undefined {
+    return this.head?.value;
+  }
+
+  print(): void {
+    let current = this.head;
+    while (current) {
+      console.log(current.value);
+      current = current.next;
+    }
+  }
+}
+
+const list = new Queue<number>();
+list.enqueue(1);
+list.enqueue(3);
+list.enqueue(0);
+list.print();
