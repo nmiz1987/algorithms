c1cc90ecff64237e1ec1e8a0a6603be3244b868b
Netanel Mizrahi <nmiz1987@gmail.com>
2024-05-02 08:24:55 +0300

    add MazeSolver

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+const config = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 80,
+  tabWidth: 2,
+};
+
+module.exports = config;
