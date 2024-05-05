d5bcbde95550bd376c0d497eb2c57032dd3f2364
Netanel Mizrahi <nmiz1987@gmail.com>
2024-05-05 08:20:47 +0300

    add QuickSort

---
+++ QuickSort/QuickSort.test.ts
@@ -0,0 +1,10 @@
+import { expect, test } from "@jest/globals";
+import QuickSort from "./QuickSort";
+
+
+test("quick-sort", function () {
+    const arr = [9, 3, 7, 4, 69, 420, 42];
+    console.log(arr);
+    QuickSort(arr);
+    expect(arr).toEqual([3, 4, 7, 9, 42, 69, 420]);
+});
