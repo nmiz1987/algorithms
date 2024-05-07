1362cba91fb74fa7bb62d4da23a25c72fee88e71
Netanel Mizrahi <nmiz1987@gmail.com>
2024-05-07 18:43:16 +0300

    add DFSonBST

--- src/DFSOnBST/DFSOnBST.ts
+++ src/DFSOnBST/DFSOnBST.ts
@@ -17,3 +17,5 @@ function search(curr: BinaryNode<number> | null, needle: number) {
 export default function dfs(head: BinaryNode<number>, needle: number): boolean {
     return search(head, needle);
 }
+
+// O(log(h)) - h is the height of the tree
