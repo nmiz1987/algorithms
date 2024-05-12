ba4520b2e5228a22ae10eb936699d7a691a914b8
Netanel Mizrahi <nmiz1987@gmail.com>
2024-05-12 08:18:41 +0300

    add DFSGraphList

---
+++ src/DFSGraphList/DFSGraphList.test.ts
@@ -0,0 +1,9 @@
+import { expect, test } from "@jest/globals";
+import dfs from "./DFSGraphList";
+import { list2 } from "../graph";
+
+test("dfs - graph", function () {
+    expect(dfs(list2, 0, 6)).toEqual([0, 1, 4, 5, 6]);
+
+    expect(dfs(list2, 6, 0)).toEqual(null);
+});
